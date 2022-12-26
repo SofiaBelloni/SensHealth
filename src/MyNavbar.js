69d9f7746c75210eec0ae04f1ff6df6b5650a2ec
claudione996 <s295829@studenti.polito.it>
2022-12-26 18:33:01 +0100

    added navbar

---
+++ src/MyNavbar.js
@@ -0,0 +1,50 @@
+import { Navbar, Nav} from "react-bootstrap/";
+import { PersonCircle,List } from "react-bootstrap-icons";
+import "./MyNavbar.css";
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+function MyNavbar () {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  return <>
+    <Navbar id='navbar' variant='light' fixed='top' className='px-3 d-flex flex-row justify-content-between'>
+      <Navbar.Toggle aria-controls='left-sidebar'/>
+      <Button variant='light'onClick={handleShow}>
+        <List size={20}/>
+      </Button>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Menu</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+        <Nav defaultActiveKey="/home" className="flex-column">
+      <Nav.Link href="/home">Call list</Nav.Link>
+      <Nav.Link eventKey="link-1">Sensor stauts</Nav.Link>
+        </Nav>       
+     </Offcanvas.Body>
+      </Offcanvas>
+        <Navbar.Brand id='navbar-brand' className="text-align-center">SenseHealth</Navbar.Brand>
+    
+      <Nav className='ml-auto'>
+        <Nav.Item>
+         
+            <div className='d-flex flex-row align-items-center'>
+              <h5 id='nav-username' className='mx-4 my-0'>
+                Hi,Mario!
+                
+              </h5>
+              <PersonCircle size={40}/>
+            </div>
+          
+        </Nav.Item>
+      </Nav>
+    </Navbar>
+    </>
+};
+
+export { MyNavbar };
